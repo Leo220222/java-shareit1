1a4e466f1034eba31714a5260ceb5578f5b6a6d2
Leo220222 <92219283+Leo220222@users.noreply.github.com>
2023-03-17 11:32:36 +0500

    Add files via upload

--- src/main/java/ru/practicum/shareit/item/CommentMapper.java
+++ src/main/java/ru/practicum/shareit/item/CommentMapper.java
@@ -4,16 +4,20 @@ import org.springframework.stereotype.Service;
 import ru.practicum.shareit.item.dto.CommentDto;
 import ru.practicum.shareit.item.model.Comment;
 import ru.practicum.shareit.user.User;
+import ru.practicum.shareit.user.UserRepository;
 
 @Service
 public class CommentMapper {
+
+    private UserRepository userRepository;
+    private ItemRepository itemRepository;
 
     public CommentDto toDto(Comment comment, User user) {
         CommentDto commentDto = new CommentDto();
         commentDto.setId(comment.getId());
         commentDto.setText(comment.getText());
-        commentDto.setItem(comment.getItem());
-        commentDto.setAuthor(comment.getAuthor());
+        commentDto.setItem(comment.getItem().getId());
+        commentDto.setAuthor(comment.getAuthor().getId());
         commentDto.setAuthorName(user.getName());
         commentDto.setCreated(comment.getCreated());
         return commentDto;
@@ -22,8 +26,8 @@ public class CommentMapper {
     public Comment toComment(CommentDto commentDto) {
         Comment comment = new Comment();
         comment.setText(commentDto.getText());
-        comment.setItem(commentDto.getItem());
-        comment.setAuthor(commentDto.getAuthor());
+        comment.setItem(itemRepository.findById(commentDto.getItem()).orElseThrow());
+        comment.setAuthor(userRepository.findById(commentDto.getAuthor()).orElseThrow());
         return comment;
     }
 }
